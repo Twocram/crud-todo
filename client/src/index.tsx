52a4f47d29f84f6492f0a08f35012dc50d91b2ee
Twocram <2002maslovartem@gmail.com>
2022-07-15 13:58:52 +0500

    refactored file

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Error from './pages/404';
+import Auth from './pages/Auth';
+import Home from './pages/Home';
+import Reg from './pages/Reg';
+import Layout from './Layout';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path='/home/:id' element={<Home />} />
+          <Route path='/reg' element={<Reg />} />
+          <Route path='/auth' element={<Auth />} />
+          <Route path='*' element={<Error />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   </React.StrictMode>
 );
