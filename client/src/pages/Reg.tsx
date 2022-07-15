f91eea694ebbe27df90d7caa60b22e52ac749301
Twocram <2002maslovartem@gmail.com>
2022-07-15 14:53:38 +0500

    created registration page

--- client/src/pages/Reg.tsx
+++ client/src/pages/Reg.tsx
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { ChangeEvent, FC, useState } from 'react';
+import Form from '../components/Form/Form';
+import { IUser } from '../types/types';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 type Props = {};
 
-function Reg(props: Props) {
-  return <div>Reg page</div>;
-}
+const Reg: FC<Props> = () => {
+  const [user, setUser] = useState<IUser>({
+    name: '',
+    password: '',
+  });
+  const navigate = useNavigate();
+
+  const createUser = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:3001/create', {
+        name: user.name,
+        password: user.password,
+      })
+      .then(() => {
+        console.log('user created');
+        navigate('/auth');
+      });
+    setUser({
+      name: '',
+      password: '',
+    });
+  };
+
+  return (
+    <>
+      <Form>
+        <input
+          type='text'
+          placeholder='Name...'
+          value={user.name}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setUser({ ...user, name: e.target.value })
+          }
+        />
+        <input
+          type={'password'}
+          placeholder='Password...'
+          value={user.password}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setUser({ ...user, password: e.target.value })
+          }
+        />
+        <button onClick={createUser}>Register</button>
+      </Form>
+    </>
+  );
+};
 
 export default Reg;
